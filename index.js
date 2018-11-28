87d363cc71136255457caf3a45b842d83dcb722d
unknown <chaquita@cjmdevelopments.com>
2018-11-27 18:32:40 -0600

    adding GREET

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ import Navigation from './src/Navigation';
 import Header from './src/Header';
 import Content from './src/Content';
 import Footer from './src/Footer';
+import Greet from './src/Greet';
 
 document.querySelector('#root').innerHTML = `
     ${Navigation}
     ${Header}
     ${Content}
     ${Footer}
+    ${Greet}
     `;
 
 
 
 //var title = document.querySelector('h1');
 
-//var greet = function greet(){
+//function greet(){
   //  if(!userName){
     //    userName = prompt('What is your name, for real this time?');
         
@@ -25,5 +27,5 @@ document.querySelector('#root').innerHTML = `
     //}
 //};
 
-// var userName = prompt('What is your name?');
+// userName = prompt('What is your name?');
 // greet();
